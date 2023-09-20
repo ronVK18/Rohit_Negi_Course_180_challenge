70cc3611417edf221ef75dc9b935631cd3a854dd
ronVK18 <143346378+ronVK18@users.noreply.github.com>
2023-09-20 19:46:44 +0530

    Add files via upload

---
+++ DAY-3 20.09.2023/ans_file1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    //Problem - 1
+    // Given two number and to print their product
+    int a=10,b=20;
+    cout << a <<" * " << b <<" = "<< a*b << endl;
+
+    // Problem - 2
+    // Given two number and to print their subtraction
+    int c=16,d=7;
+    cout << c <<" - " << d <<" = "<< c-d << endl;
+   
+    //Problem - 3
+    //to print only 6*3 not 18
+    cout << " 6 * 3 "<<endl;
+    //other question at notebook
+    
+
+    return 0;
+}
